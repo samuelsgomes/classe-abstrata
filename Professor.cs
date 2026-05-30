88deedb16bf3fd7f8073130a9cb28f707d4de041
samuelsgomes <samuel.s.gomes11@aluno.senai.br>
2026-05-30 12:13:59 -0300

    Update Professor.cs

--- Professor.cs
+++ Professor.cs
@@ -1,7 +1,9 @@
 namespace Documents.classe_usando_abstract
 {
-    public class Professor
+    public class Professor : Pessoa
     {
-        
+        public override void Apresentar(string nome, int idade)
+    }
+        Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos e sou professor.");
     }
 }
