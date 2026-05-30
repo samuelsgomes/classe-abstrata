70ff2d66d5c25c74a2dc0df914fe40668c132755
samuelsgomes <samuel.s.gomes11@aluno.senai.br>
2026-05-30 12:19:05 -0300

    Update Aluno.cs

--- Aluno.cs
+++ Aluno.cs
@@ -2,6 +2,8 @@ namespace classe_usando_abstract
 {
     public class Aluno
     {
-        
+        public override void Apresentar(string nome, int idade)
+    {
+     Console.WriteLine($"Olá, meu nome é {Nome}, tenho {Idade} anos ")
     }
 }
